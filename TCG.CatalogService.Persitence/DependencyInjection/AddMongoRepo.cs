719707fb8e4853e6a1c703d1cf41d41440988ade
val <valentin.bonnefon@diiage.org>
2023-06-12 13:16:54 +0200

    Running dev

--- TCG.CatalogService.Persitence/DependencyInjection/AddMongoRepo.cs
+++ TCG.CatalogService.Persitence/DependencyInjection/AddMongoRepo.cs
@@ -12,25 +12,6 @@ namespace TCG.CatalogService.Persitence.DependencyInjection;
 public static class AddMongoRepo
 {
     //Register the IMongoDatabse Instance
-    public static IServiceCollection AddMongo(this IServiceCollection services)
-    {
-        BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
-        BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-
-        //On enregistre notre bdd avec localhost + port + création bdd avec Catalog en nom
-        services.AddSingleton(sericeProvider =>
-        {
-            var config = sericeProvider.GetService<IConfiguration>();
-            //On récupère le nom de la table Catalog
-            
-            var serviceSettigns = config.GetSection("ServiceSettings").Get<ServiceSettings>();
-            var mongoDbSettings = config.GetSection("MongoDbSettings").Get<MongoDbSettings>();
-            var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
-            return mongoClient.GetDatabase(serviceSettigns.ServiceName);
-        });
-        return services;
-    }
-
     public static IServiceCollection AddMongoPersistence(this IServiceCollection services, IConfiguration configuration)
     {
         // Enregistrer IMongoDatabase
